e9b0f01c5c1334dc76a046a59e5bf18009d2f948
Bilal <19686500+kobi87@users.noreply.github.com>
2020-05-07 12:48:37 -0400

    Add files via upload

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,12 @@
+<template>
+    <span>
+        <v-toolbar app color="teal lighten-2" dark>
+            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <img src="../assets/white-labs-plain.png" height="50" />
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <!-- <v-btn flat class="hidden-sm-and-down" text rounded>HOME</v-btn>
+            <v-btn flat class="hidden-sm-and-down" text rounded>LOGIN</v-btn> -->
+        </v-toolbar>
+    </span>
+</template>
